c5aa0bc651bf7193bc11a8bce3d230baa6152a8d
Lily Lawliet <dr.trayaurus61@gmail.com>
2025-11-30 12:44:40 -0800

    update MarioPouch

--- include/cs/marioPouch.h
+++ include/cs/marioPouch.h
@@ -5,6 +5,19 @@
 
 CPP_WRAPPER(cs::mario_pouch)
 
-void SetCoin(int count);
+typedef struct {
+  s32 red;
+  s32 yellow;
+  s32 blue;
+} GameInk;
+
+s32 GetCoin();
+void SetCoin(s32 count);
+void AddCoin(s32 count);
+void AddKeyItem(const char *key);
+s32 GetMaxHP();
+s32 GetHP();
+void SetHP(s32 hp);
+void GetInk(GameInk * ink);
 
 CPP_WRAPPER_END()
